89f131d3c38bfd739be38633975d08847e7bf674
loren-zhihao <2415612109@qq.com>
2024-01-15 00:21:01 +0800

    获取九转连环可能得组合

--- src/main/java/com/pj/utils/IntegerUtils.java
+++ src/main/java/com/pj/utils/IntegerUtils.java
@@ -2,6 +2,9 @@ package com.pj.utils;
 
 import org.springframework.util.StringUtils;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class IntegerUtils {
 
     public static String convertToString(Integer code){
@@ -37,6 +40,32 @@ public class IntegerUtils {
             return 0 > b;
         }
         return Integer.valueOf(a) > b;
+    }
+
+    /**
+     * 获取九转连环可能得组合
+     * @return
+     */
+    public static List<String> getNineCompose(){
+        List<String> list = new ArrayList<>(500);
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                for (int k = 0; k < 5; k++) {
+                    for (int l = 0; l < 5; l++) {
+                        if(i+j+k+l != 6){
+                            continue;
+                        }
+                        list.add(i+"_"+j+"_"+k+"_"+l);
+                    }
+                }
+            }
+        }
+        return list;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(getNineCompose());
+        System.out.println(getNineCompose().size());
     }
 
 }
